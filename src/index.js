2b70c74f7e8f07b5203fb5adea61221fdfe9ac25
Leyna <elena-voytovich@yandex.ru>
2022-08-17 10:22:37 +0300

    add tags style change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const TagsWrapper = document.querySelector('.strategies__tags');
+const tagsWrapper = document.querySelector('.strategies__tags');
+const tags = tagsWrapper.querySelectorAll('.tag');
 
 window.onload = function () {
     console.log('I am loaded');
@@ -7,14 +8,24 @@ window.onload = function () {
 }
 
 const addTagsClickHandler = () => {
-    TagsWrapper.addEventListener('click', (evt) => {
+    tagsWrapper.addEventListener('click', (evt) => {
         console.log(evt);
-        if (evt.target.classList.contains('.tag')) {
+        if (evt.target.classList.contains('tag')) {
+            let clickedTag = evt.target;
             removeSelectedTags();
+            selectClickedTag(clickedTag);
         }
     })
 }
 
 const removeSelectedTags = () => {
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    })
+}
 
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.remove('tag_bordered');
+    clickedTag.classList.add('tag_selected');
 }
